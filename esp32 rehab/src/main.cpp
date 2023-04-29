23a422d1ac055d680f106c6b9f9b2f60e0e8fcce
Thomas <131818418+ThoMiranda@users.noreply.github.com>
2023-04-29 20:15:56 -0300

    Add files via upload

--- esp32 rehab/src/main.cpp
+++ esp32 rehab/src/main.cpp
@@ -2,19 +2,20 @@
 #include "secuencias.h"
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel strip(49, 2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(49, 16, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
   // Initialize LED strip
   Serial.begin(115200);  
   strip.setBrightness(20);
-  strip.begin();
-  strip.show();
+  strip.begin(); 
+  strip.clear();
 }
 
 void loop() {
   // Call flechas function with "arriba" direction and red color
-  flechas('d', 1, strip);
-  Serial.println("Arriba");
+flechas('d', 1, strip);
+
+
 }
--- esp32 rehab/src/secuencias.cpp
+++ esp32 rehab/src/secuencias.cpp
@@ -3,85 +3,92 @@
 void flechas(char direccion, int color, Adafruit_NeoPixel tira){
     int r, g, b; 
 
-    if (color = 0){
+    if (color == 0){
         r=255;
         g=0;
         b=0;
-    } else if(color=1) {
+    } else if(color==1) {
         r=0;
         g=255;
         b=0;
-    }  else if(color=2){
+    }  else if(color==2){
         r=0;
         g=0;
         b=255;
     }
-    tira.setBrightness(20);
     tira.begin();
     switch(direccion){
         case 'u':
             //ARRIBA
             tira.clear();
-            tira.setPixelColor(3,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(9,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(10,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(11,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(15,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(17,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(19,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(24,r,g,b);// posicion (0,R G B)  
-            tira.setPixelColor(31,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(38,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(45,r,g,b);// posicion (0,R G B)
+            tira.setPixelColor(3, r, g, b);
+            tira.setPixelColor(9, r, g, b);
+            tira.setPixelColor(10, r, g, b);
+            tira.setPixelColor(11, r, g, b);
+            tira.setPixelColor(15, r, g, b);
+            tira.setPixelColor(17, r, g, b);
+            tira.setPixelColor(19, r, g, b);
+            tira.setPixelColor(21, r, g, b);
+            tira.setPixelColor(24, r, g, b);
+            tira.setPixelColor(27, r, g, b);
+            tira.setPixelColor(31, r, g, b);
+            tira.setPixelColor(38, r, g, b);
+            tira.setPixelColor(45, r, g, b);
             tira.show();
-
             break;
+
         case 'd':
-              //ABAJO
             tira.clear();
-            tira.setPixelColor(3,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(10,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(17,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(24,r,g,b);// posicion (0,R G B)  
-            tira.setPixelColor(31,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(38,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(39,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(37,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(33,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(29,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(45,r,g,b);// posicion (0,R G B)
+            tira.setPixelColor(3, r, g, b);
+            tira.setPixelColor(10, r, g, b);
+            tira.setPixelColor(17, r, g, b);
+            tira.setPixelColor(21, r, g, b);
+            tira.setPixelColor(24, r, g, b);
+            tira.setPixelColor(27, r, g, b);
+            tira.setPixelColor(29, r, g, b);
+            tira.setPixelColor(31, r, g, b);
+            tira.setPixelColor(33, r, g, b);
+            tira.setPixelColor(37, r, g, b);
+            tira.setPixelColor(38, r, g, b);
+            tira.setPixelColor(39, r, g, b);
+            tira.setPixelColor(45, r, g, b);
+            tira.show();
+            break;
+
+        case 'r':
+            //DERECHA
+            tira.clear();
+            tira.setPixelColor(3, r, g, b);
+            tira.setPixelColor(9, r, g, b);
+            tira.setPixelColor(19, r, g, b);
+            tira.setPixelColor(21, r, g, b);
+            tira.setPixelColor(22, r, g, b);
+            tira.setPixelColor(23, r, g, b);
+            tira.setPixelColor(24, r, g, b);
+            tira.setPixelColor(25, r, g, b);
+            tira.setPixelColor(26, r, g, b);
+            tira.setPixelColor(27, r, g, b);
+            tira.setPixelColor(33, r, g, b);
+            tira.setPixelColor(37, r, g, b);
+            tira.setPixelColor(45, r, g, b);            
+            tira.show();
             break;
         case 'l':
             //IZQUIERDA
             tira.clear();
-            tira.setPixelColor(21,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(22,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(23,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(24,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(25,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(26,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(27,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(15,r,g,b);// posicion (0,R G B)  
-            tira.setPixelColor(29,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(11,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(39,r,g,b);// posicion (0,R G B)
-            tira.show();
-
-            break;
-        case 'r':
-            //DERECHA
-            tira.clear();
-            tira.setPixelColor(21,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(22,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(23,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(24,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(25,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(26,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(27,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(33,r,g,b);// posicion (0,R G B)  
-            tira.setPixelColor(19,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(9,r,g,b);// posicion (0,R G B)
-            tira.setPixelColor(37,r,g,b);// posicion (0,R G B)
+            tira.setPixelColor(3, r, g, b);
+            tira.setPixelColor(11, r, g, b);
+            tira.setPixelColor(15, r, g, b);
+            tira.setPixelColor(21, r, g, b);
+            tira.setPixelColor(22, r, g, b);
+            tira.setPixelColor(23, r, g, b);
+            tira.setPixelColor(24, r, g, b);
+            tira.setPixelColor(25, r, g, b);
+            tira.setPixelColor(26, r, g, b);
+            tira.setPixelColor(27, r, g, b);
+            tira.setPixelColor(29, r, g, b);
+            tira.setPixelColor(39, r, g, b);
+            tira.setPixelColor(45, r, g, b);            
             tira.show();
             break;
     }
