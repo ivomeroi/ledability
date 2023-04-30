a8f2ff49f8809b7b1fbaf2913bdc951cfb27f891
Thomas Miranda <thomas.miranda@mi.unc.edu.ar>
2023-04-30 20:18:23 -0300

    enviado

--- esp32 rehab/src/transmision.cpp
+++ esp32 rehab/src/transmision.cpp
@@ -14,13 +14,27 @@ typedef struct var_struct {
 var_struct dupla;
 
 void peering(uint8_t direcciones[][6]){
-
   // register peer
-    esp_now_peer_info_t peerInfo;
-    peerInfo.channel = 0;  
-    peerInfo.encrypt = false;
-    
+    esp_now_peer_info_t peer[2];
+
     for (int i = 0; i < 2; i++) {
-        memcpy(peerInfo.peer_addr, direcciones[i], 6);
+        peer[i].channel = 0;  
+        peer[i].encrypt = false;
+        memcpy(peer[i].peer_addr, direcciones[i], 6);
+        if (esp_now_add_peer(&peer[i]) != ESP_OK){
+            Serial.println("Failed to add peer");
+            return;
+        }
+    }
+}
+
+void sendpads(uint8_t direcciones[][6], var_struct dupla){
+    // send data
+    esp_err_t result = esp_now_send(direcciones[0], (uint8_t *) &dupla, sizeof(dupla));
+    if (result == ESP_OK) {
+        Serial.println("Sent with success");
+    }
+    else {
+        Serial.println("Error sending the data");
     }
 }
