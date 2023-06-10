a148b97e8d566ae17cc1d35cd04c952990e5c330
Thomas Miranda <thomas.miranda@mi.unc.edu.ar>
2023-06-10 14:58:34 -0300

    writeBT update

--- esp32 rehab/src/sensor.cpp
+++ esp32 rehab/src/sensor.cpp
@@ -19,9 +19,26 @@ void initIR(int Pin){
 //-------------------------------------------------------------
 void apagarSecuencia(){
     sensorValue = digitalRead(sensorPin);// lee el valor del sensor
+    char simbolo;
       if (sensorValue==LOW) {
         tira.clear();
+
+            if (randomValue==true){
+                int numb = random(0, 10);
+                simbolo = patron[numb]; // envio el elemento X (según en contador) del array patron
+            }
+            else{
+                simbolo = patron[counter]; // envio el elemento X (según en contador) del array patron
+            }
+
+        enviar.color = BTmessage.color;  // cambiar a BTmessage.color con la App
+            if (BTmessage.color = 4){
+                enviar.color = random(0, 4);
+            }
         counter++;
+        if (counter>=strlen(patron)){ //controlo el contador. Si llega al final del array, lo reinicio y se repite el patron.
+            counter=0;
+        }
         patrones(patron[counter],BTmessage.color);
        // Serial.print('1');
        // Serial.print(patron[counter]);
@@ -29,6 +46,7 @@ void apagarSecuencia(){
        // Serial.print('\n');
         } 
     } 
+    
 //La función principal es leer constantemente la entrada del sensor. Si la salida es LOW, hay un obstaculo y el la variable toMaster 
 //(la que irá del slave al master) se pondrá en true. Entonces esta información irá al master. Luego se limpian las tiras y se espera a que lleguen nuevos valores
 //Al final, se reestablece  el valor de la variable toMaster a False. 
